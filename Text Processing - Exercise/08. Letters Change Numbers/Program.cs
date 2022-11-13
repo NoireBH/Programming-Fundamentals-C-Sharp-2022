9611e0b5abaf8335267afc70423ed30a3d5c9aec
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-11-13 16:19:32 +0200

    added solutions to projects

---
+++ Text Processing - Exercise/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _08._Letters_Change_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numberBetweenTwoLetters = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            double sum = 0;
+
+            foreach (var word in numberBetweenTwoLetters)
+            {
+                
+                char firstLetter = word[0];
+                char lastLetter = word[word.Length - 1];
+                double number = double.Parse(word[1..^1]);
+
+                if (char.IsUpper(firstLetter))
+                {
+                    int positionOfLetter = firstLetter - 64;
+                    number /= positionOfLetter;
+                }
+                else
+                {
+                    int positionOfLetter = firstLetter - 96;
+                    number *= positionOfLetter;
+                }
+
+                if (char.IsUpper(lastLetter))
+                {
+                    int positionOfLetter = lastLetter - 64;
+                    number -= positionOfLetter;
+                }
+                else
+                {
+                    int positionOfLetter = lastLetter - 96;
+                    number += positionOfLetter;
+                }
+                sum += number;      
+            }
+            Console.WriteLine($"{sum:f2}");
+
+
+        }
+
+
+    }
+}
