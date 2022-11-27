f34a12befc4fcc30fdbf9e0ae9aeef86a852107e
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2022-11-27 15:10:23 +0200

    updated solution

--- Programming Fundamentals Final Exam Exercise/02. Ad Astra/Program.cs
+++ Programming Fundamentals Final Exam Exercise/02. Ad Astra/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace _02._Ad_Astra
@@ -6,37 +7,27 @@ namespace _02._Ad_Astra
     internal class Program
     {
         static void Main(string[] args)
-        {   
-            int totalCalories = 0;
-            int caloriesDIvided = 0;
-            int count = 0;
-            string pattern = @"(#|\|)(?<name>[A-za-z\s]+)\1(?<expiration>(?<day>\d{2})\/(?<month>\d{2})\/(?<year>\d{2}))\1(?<calories>\d+)\1";
+        {
             string text = Console.ReadLine();
+            string pattern = @"(\||#)(?<name>[A-za-z\s]+)\1(?<expiration>(?<day>\d{2})\/(?<month>\d{2})\/(?<year>\d{2}))\1(?<calories>\d+)\1";
             Regex regex = new Regex(pattern);
-
             MatchCollection matches = regex.Matches(text);
-                
-            foreach (Match match in matches)
+            int totalCalories = 0;
+            int count = 0;
+            foreach (Match item in matches)
             {
-                int calories = int.Parse(match.Groups["calories"].Value);
 
+                int calories = int.Parse(item.Groups["calories"].Value);
                 totalCalories += calories;
+
+
             }
-                
 
-                
-                
-            
-
-            caloriesDIvided = totalCalories;
-
-            while (caloriesDIvided >= 2000)
+            while (totalCalories >= 2000)
             {
-                caloriesDIvided -= 2000;
+                totalCalories -= 2000;
                 count++;
             }
-                
-            
 
             Console.WriteLine($"You have food to last you for: {count} days!");
 
@@ -47,8 +38,6 @@ namespace _02._Ad_Astra
                 int calories = int.Parse(item.Groups["calories"].Value);
                 Console.WriteLine($"Item: {name}, Best before: {expiration}, Nutrition: {calories}");
             }
-                
-            
         }
     }
 }
